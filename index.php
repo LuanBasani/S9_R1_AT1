591ff3bebadbd0ee096ebbe208c378401ebe39d2
LuanBasani <luanbasani@gmail.com>
2026-05-12 11:07:06 -0300

    12/05/26 - Arrumando e melhorando

--- index.php
+++ index.php
@@ -1,40 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulário</title>
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <form method="post">
+
+<div class="container">
+
+    <h2>Contato</h2>
+
+    <form action="processa.php" method="post">
+
         <label>Nome:</label>
-        <input type="text" name="nome"> <br>
+        <input type="text" name="nome" required>
 
         <label>Email:</label>
-        <input type="email" name="email"> <br>
+        <input type="email" name="email" required>
 
-
-        <label>Texto:</label>
-        <input type="text" name="texto"> <br>
+        <label>Mensagem:</label>
+        <textarea name="texto" rows="5" required></textarea>
 
         <input type="submit" value="Enviar">
+
     </form>
 
-    <h2>Informações Enviadas</h2>
-    <hr>
+</div>
 
-    <?php 
-        // Função para exibir os dados recebidos pelo formulário
-        function exibirCampo($label, $valor) {
-            echo "$label: $valor <br>";
-        }
-
-        // Verifica se o formulário foi enviado antes de exibir os dados
-        if (isset($_POST["nome"], $_POST["email"], $_POST["texto"])) {
-            exibirCampo("Nome", $_POST["nome"]);
-            exibirCampo("Email", $_POST["email"]);
-            exibirCampo("Texto", $_POST["texto"]);
-        }
-    ?>
 </body>
 </html>
